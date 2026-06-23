dd71efeed577604377dbb15c9ea318b877b76725
Anas Nammas <anas@codexkw.com>
2026-06-24 00:10:29 +0300

    feat: extend Vehicle with rich-section fields and navigations

--- Solution/GAC.Core/Content/Vehicle.cs
+++ Solution/GAC.Core/Content/Vehicle.cs
@@ -20,9 +20,27 @@ public class Vehicle
     public LocalizedText MetaTitle { get; set; } = new();
     public LocalizedText MetaDescription { get; set; } = new();
 
+    // Rich-section scalar/localized fields
+    public string? TechBannerImage { get; set; }
+    public LocalizedText StatsNote { get; set; } = new();
+    public string? EnquiryBgImage { get; set; }
+    public LocalizedText EnquiryTitle { get; set; } = new();
+    public LocalizedText EnquirySub { get; set; } = new();
+    public LocalizedText EnquiryLead { get; set; } = new();
+
     public List<VehicleImage> Images { get; set; } = new();
     public List<Trim> Trims { get; set; } = new();
     public List<SpecGroup> SpecGroups { get; set; } = new();
     public List<ColorOption> Colors { get; set; } = new();
     public List<FeatureSection> Features { get; set; } = new();
+
+    // Rich-section collections
+    public List<SectionHeading> Headings { get; set; } = new();
+    public List<StatItem> Stats { get; set; } = new();
+    public List<SliderGroup> Sliders { get; set; } = new();
+    public List<GalleryTab> GalleryTabs { get; set; } = new();
+    public List<CardItem> Cards { get; set; } = new();
+    public List<SafetyToggle> SafetyToggles { get; set; } = new();
+    public List<WarrantyLink> WarrantyLinks { get; set; } = new();
+    public QualityBlock? Quality { get; set; }
 }
